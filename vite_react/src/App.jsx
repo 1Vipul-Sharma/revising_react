68502534b9c95a0a99f67e2ac36059d34c6f34a6
Your Name <you@example.com>
2025-03-11 07:55:51 +0530

    first commit

---
+++ vite_react/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>VITE + REACT</h1>
+  )
+}
+
+export default App
---
+++ webpack_react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+  <h1>WEBPACK + REACT </h1>
+  );
+}
+
+export default App;
